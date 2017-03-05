c2c939828948e9d5b20740120cb07486f39d3da8
kaduc <kaduc@DESKTOP-PKF1K62>
2017-03-05 17:37:22 -0300

    Versão sem relatórios, modificação da classe de dao da entrada de prontuario

--- SISPRONT/src/cadastro/sispront/entradadao.java
+++ SISPRONT/src/cadastro/sispront/entradadao.java
@@ -18,12 +18,12 @@ import persistencia.sispront.dao;
 public class entradadao extends dao{
     public void gravar (entrada e) throws Exception{
         Conexao();
-        String sql = "insert into entrega_prontuario (codfunc, codpront, data, hora) values (?, ?, ?, ?)";
+        String sql = "insert into entrega_prontuario (codfunc, codpront, data, hora) values (?, ?, DATE_FORMAT(NOW(),'%d/%m/%Y'), CURTIME())";
         stmt1 = con.prepareStatement(sql);
         stmt1.setInt(1, e.getCodfunc());
         stmt1.setInt(2, e.getCodpront());
-        stmt1.setString(3, e.getDt());
-        stmt1.setString(4, e.getHr());
+        //stmt1.setString(3, e.getDt());
+        //stmt1.setString(3, e.getHr());
 
         stmt1.executeUpdate();
         stmt1.close();
